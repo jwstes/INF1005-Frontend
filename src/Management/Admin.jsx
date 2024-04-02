9e936d6ad47c1b03f57bad2d8ba2f2a57e9d8f5d
IXaturn <gmingquan96@gmail.com>
2024-04-02 21:36:14 +0800

    Created Nav Bar for Management Page

--- src/Management/Admin.jsx
+++ src/Management/Admin.jsx
@@ -1,32 +1,53 @@
 // AdminPage.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import ProductList from './ProductList';
 import ProductForm from './ProductForm';
 import UpdateProductForm from './UpdateProductForm';
 import DeleteProductForm from './DeleteProductForm';
-
 import './AdminPage.css'; 
 
 const AdminPage = () => {
+  const [selectedOption, setSelectedOption] = useState('productList'); // Default selected option
+  
+  const handleOptionClick = (option) => {
+    setSelectedOption(option);
+  };
+
   return (
     <div className="admin-container">
-      <div className="product-list">
-        <h1>Product List</h1>
-        <ProductList />
+      <div className="sidebar">
+        <ul className="sidebar-menu">
+          <li className={selectedOption === 'productList' ? 'active' : ''} onClick={() => handleOptionClick('productList')}>View Product List</li>
+          <li className={selectedOption === 'createProduct' ? 'active' : ''} onClick={() => handleOptionClick('createProduct')}>Create Product</li>
+          <li className={selectedOption === 'updateProduct' ? 'active' : ''} onClick={() => handleOptionClick('updateProduct')}>Update Product</li>
+          <li className={selectedOption === 'deleteProduct' ? 'active' : ''} onClick={() => handleOptionClick('deleteProduct')}>Delete Product</li>
+        </ul>
       </div>
-      <div className="forms-container">
-        <div className="product-form">
-          <h1>Create Product</h1>
-          <ProductForm />
-        </div>
-        <div className="update-product-form">
-          <h1>Update Product</h1>
-          <UpdateProductForm />
-        </div>
-        <div className="delete-product-form">
-          <h1>Delete Product</h1>
-          <DeleteProductForm />
-        </div>
+      <div className="content">
+        {selectedOption === 'productList' && (
+          <div className="product-list">
+            <h1>Product List</h1>
+            <ProductList />
+          </div>
+        )}
+        {selectedOption === 'createProduct' && (
+          <div className="product-form">
+            <h1>Create Product</h1>
+            <ProductForm />
+          </div>
+        )}
+        {selectedOption === 'updateProduct' && (
+          <div className="update-product-form">
+            <h1>Update Product</h1>
+            <UpdateProductForm />
+          </div>
+        )}
+        {selectedOption === 'deleteProduct' && (
+          <div className="delete-product-form">
+            <h1>Delete Product</h1>
+            <DeleteProductForm />
+          </div>
+        )}
       </div>
     </div>
   );
